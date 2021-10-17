9a3c77ff0b27aa17e86bce03f7eebd554294eb72
Gaurav Garia <gaurav.garia@outlook.com>
2021-10-17 17:24:48 +0530

    array module added

---
+++ src/array/Array.java
@@ -0,0 +1,17 @@
+package array;
+
+public class Array {
+
+    private String[] names = new String[2];
+
+    public void showNames(){
+
+            names[0] = "gaurav";
+            names[1] = "nimesh";
+
+            for(String name:names)
+            System.out.println(name);
+
+    }
+
+}
--- src/dispatch/Empty.java
+++ src/dispatch/Empty.java
@@ -1,4 +1,16 @@
 package dispatch;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Empty {
+    public static void main(String[] args) {
+        Date date = new Date();
+        java.sql.Date sqlDate = new java.sql.Date(date.getTime());
+        java.sql.Time time = new java.sql.Time(Calendar.getInstance().getTime().getTime());
+        System.out.println(sqlDate);
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        System.out.println(timestamp);
+    }
 }
